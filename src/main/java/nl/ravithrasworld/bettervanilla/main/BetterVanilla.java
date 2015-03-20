6d909c9235670e7ce7e13b0d24f7e42636b3a407
Ravi van Rooijen <github@ravithrasworld.nl>
2015-03-20 14:18:37 +0100

    Added recipe for packed ice
    Added an recipe for packed ice in the following formation:
    II
    II
    (I = ice, output = 2 packed ice)

--- src/main/java/nl/ravithrasworld/bettervanilla/main/BetterVanilla.java
+++ src/main/java/nl/ravithrasworld/bettervanilla/main/BetterVanilla.java
@@ -14,7 +14,7 @@ import nl.ravithrasworld.bettervanilla.util.ConfigHelper;
 @Mod(modid = BetterVanilla.MODID, version = BetterVanilla.VERSION)
 public class BetterVanilla {
     public static final String MODID = "BetterVanilla";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     
     @EventHandler
     public void init(FMLInitializationEvent event) {
@@ -42,6 +42,10 @@ public class BetterVanilla {
 				new ItemStack(Items.string, 1),
 				new ItemStack(Items.dye, 1, 0),
 			});
+		}
+		
+		if (ConfigHelper.recipePackedIce) {
+			GameRegistry.addRecipe(new ItemStack(Blocks.packed_ice, 2), "II", "II", 'I', Blocks.ice);
 		}
 		
 		if (ConfigHelper.recipeChainArmor) {
--- src/main/java/nl/ravithrasworld/bettervanilla/util/ConfigHelper.java
+++ src/main/java/nl/ravithrasworld/bettervanilla/util/ConfigHelper.java
@@ -1,59 +1,76 @@
 package nl.ravithrasworld.bettervanilla.util;
 
-import org.apache.logging.log4j.Logger;
+import java.util.ArrayList;
 
-import org.apache.logging.log4j.Level;
-
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.ShapedRecipes;
+import net.minecraft.item.crafting.ShapelessRecipes;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
 
 public class ConfigHelper {
 
 	public ConfigHelper() {
-		
+
 	}
-	
-	/*public static boolean requireSavingStone;
-	private static Property requireSavingStoneProp;*/
-	
+
+	/*
+	 * public static boolean requireSavingStone; private static Property
+	 * requireSavingStoneProp;
+	 */
+
 	public static boolean recipeCobWeb;
 	public static boolean recipeHorseArmor;
 	public static boolean recipeSaddle;
 	public static boolean recipeNameTag;
 	public static boolean recipeChainArmor;
+	public static boolean recipePackedIce;
 
 	private static Property recipeCobWebProp;
 	private static Property recipeHorseArmorProp;
 	private static Property recipeSaddleProp;
 	private static Property recipeNameTagProp;
 	private static Property recipeChainArmorProp;
-	
+	private static Property recipePackedIceProp;
+
 	public static void setupConfig(Configuration cfg, Logger logger) {
 		try {
-			/*requireSavingStoneProp = cfg.get("General", "requireSavingStone", true);
-			requireSavingStoneProp.comment = "Whether or not the Saving Stone is required to get a Death Chest";
-			requireSavingStone = requireSavingStoneProp.getBoolean(false);*/
-			
+			/*
+			 * requireSavingStoneProp = cfg.get("General", "requireSavingStone",
+			 * true); requireSavingStoneProp.comment =
+			 * "Whether or not the Saving Stone is required to get a Death Chest"
+			 * ; requireSavingStone = requireSavingStoneProp.getBoolean(false);
+			 */
+
 			recipeCobWebProp = cfg.get("Recipes", "recipeCobWeb", true);
 			recipeCobWeb = recipeCobWebProp.getBoolean(true);
-			
+
 			recipeHorseArmorProp = cfg.get("Recipes", "recipeHorseArmor", true);
 			recipeHorseArmor = recipeHorseArmorProp.getBoolean(true);
-			
+
 			recipeSaddleProp = cfg.get("Recipes", "recipeSaddle", true);
 			recipeSaddle = recipeSaddleProp.getBoolean(true);
-			
+
 			recipeNameTagProp = cfg.get("Recipes", "recipeNametag", true);
 			recipeNameTag = recipeNameTagProp.getBoolean(true);
-			
+
 			recipeChainArmorProp = cfg.get("Recipes", "recipeChainArmor", true);
 			recipeChainArmor = recipeChainArmorProp.getBoolean(true);
 			
-		} catch(Exception e) {
-			logger.log(Level.ERROR, "An error occured loading BetterVanilla config!");
+			recipePackedIceProp = cfg.get("Recipes", "recipePackedIce", true);
+			recipePackedIce = recipePackedIceProp.getBoolean(true);
+
+		} catch (Exception e) {
+			logger.log(Level.ERROR,
+					"An error occured loading BetterVanilla config!");
 			e.printStackTrace();
 		} finally {
-			if(cfg.hasChanged()) {
+			if (cfg.hasChanged()) {
 				cfg.save();
 			}
 		}
