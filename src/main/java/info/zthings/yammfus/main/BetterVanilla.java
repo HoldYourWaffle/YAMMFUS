27921251a25c7cc489de5c7dd123d3907cdf1e24
Ravi van Rooijen <github@ravithrasworld.nl>
2016-03-08 17:55:25 +0100

    Update workspace to 1.8-something.1563
    And removed old builds.

--- src/main/java/info/zthings/yammfus/main/BetterVanilla.java
+++ src/main/java/info/zthings/yammfus/main/BetterVanilla.java
@@ -15,7 +15,7 @@ import nl.ravithrasworld.bettervanilla.util.ConfigHelper;
 
 @Mod(modid = BetterVanilla.MODID, version = BetterVanilla.VERSION)
 public class BetterVanilla {
-    public static final String MODID = "BetterVanilla";
+    public static final String MODID = "yammfus";
     public static final String VERSION = "1.1.1.2";
     
     public static ClientProxy proxy = new ClientProxy();    
