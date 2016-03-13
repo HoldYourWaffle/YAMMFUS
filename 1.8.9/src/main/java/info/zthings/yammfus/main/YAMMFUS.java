d350fb16f69b9b0f8b018302c44f4c0ba024bbfd
Ravi van Rooijen <github@ravithrasworld.nl>
2016-03-13 10:47:11 +0100

    Forgot 1.8.9 version

--- 1.8.9/src/main/java/info/zthings/yammfus/main/YAMMFUS.java
+++ 1.8.9/src/main/java/info/zthings/yammfus/main/YAMMFUS.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 @Mod(modid = YAMMFUS.MODID, version = YAMMFUS.VERSION)
 public class YAMMFUS {
     public static final String MODID = "yammfus";
-    public static final String VERSION = "1.1.1.2";
+    public static final String VERSION = "1.0";
     
     public static ClientProxy proxy = new ClientProxy();    
     
